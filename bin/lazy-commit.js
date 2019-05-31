8769ce2277a07995d915aaf570bc49e2bb5ad487
Marc Backes <mab@mab.lu>
2019-05-31 10:19:16 +0200

    some logging

--- bin/lazy-commit.js
+++ bin/lazy-commit.js
@@ -1,8 +1,13 @@
 #! /usr/bin/env node
-let shell = require("shelljs");
-const branchName = require('current-git-branch');
-let branch = branchName();
-// let branch = shell.exec("$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')");
 
-branch = `-${branch}-`;
-console.log(' => --[lazy] ' + branch);
+let shell = require("shelljs");
+const fullBranchName = require('current-git-branch');
+let branch = fullBranchName();
+let branchCategory = branch.split('/')[0];
+let branchName = branch.split('/')[1];
+
+console.log({
+    branch,
+    branchCategory,
+    branchName
+});
